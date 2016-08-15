3b99bd12d0fb73e7c9c8262d3e93b396f4000a48
Abhijeet N. Vaidya <abhijeetnvaidya@gmail.com>
2016-08-15 22:49:21 +0530

    Prep. for Heroku push with admin dashboard.

--- public/js/client.js
+++ public/js/client.js
@@ -24,8 +24,8 @@ function ReturnUndefined() {
 }
 
 $(document).ready(function() {
-    var socket = io.connect('http://localhost:3333');
-    //var socket = io.connect('https://peaceful-taiga-36791.herokuapp.com');
+    //var socket = io.connect('http://localhost:3333');
+    var socket = io.connect('https://peaceful-taiga-36791.herokuapp.com');
 
     var player = "";
     if ($('#ONLINE_PLAYER').length) {
