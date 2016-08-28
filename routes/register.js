0f165b4c84b791f2a8b781806fac0ae3e27550de
Abhijeet N. Vaidya <abhijeetnvaidya@gmail.com>
2016-08-29 02:36:55 +0530

    Added sign-up form validation

--- routes/register.js
+++ routes/register.js
@@ -17,34 +17,60 @@ router.post('/', function(req, res, next) {
     var email     = req.body.email;
     var password  = req.body.password;
     var password2 = req.body.password2;
+    
+    function checkRegexp(i, regexp) {
+        if (!(regexp.test(i))) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
     PlayerDB.findOne({handle: handle}, function(err, player) {
         if (player !== null) {
             req.flash('error_msg', 'That handle is already taken.');
             res.redirect('/register');
         } else { // No player with that handle in the database
-            if (password === password2) {
-                var p = new PlayerDB({
-                                handle: handle,
-                                email: email,
-                                rating_obj : {rating: 1500, rd: 200, vol:0.06},  // glicko2 recommendations
-                                password: util.encrypt(password)
-                            });
-                p.save(function(err) {
-                    if (err) {
-                        next(err);
-                    } else {
-                        //console.log('New player: ' + p);
-                        req.login(p, function(err) {
-                            if (err) { return next(err); }
-                            req.flash('success_msg', 'Welcome ' + p.handle + "!");
-                            return res.redirect('/arena');
-                        });
-                    }
-                });
-            } else {
-                req.flash('error_msg', 'Re-typed password did not match the original.');
+            // User input validation
+            var valid = true;
+            valid = checkRegexp(handle, /^[-\w\.]{1,16}$/);
+            if (!valid) {
+                req.flash('error_msg', 'Bad characters in handle or it\'s too long.');
                 res.redirect('/register');
+            }
+            valid = checkRegexp(email, /^[-\w\.@]{6,40}$/);
+            if (!valid) {
+                req.flash('error_msg', 'Badly formatted email address.');
+                res.redirect('/register');
+            }
+            valid = checkRegexp(password, /^[@#%-=:\w\.\*\!\$\+\?\^\&\|\,\;]{4,32}$/);
+            if (!valid) {
+                req.flash('error_msg', 'Bad character(s) in password or it\'s too short/long.');
+                res.redirect('/register');
+            } else {
+                if (password === password2) {
+                    var p = new PlayerDB({
+                                    handle: handle,
+                                    email: email,
+                                    rating_obj : {rating: 1500, rd: 200, vol:0.06},  // glicko2 recommendations
+                                    password: util.encrypt(password)
+                                });
+                    p.save(function(err) {
+                        if (err) {
+                            next(err);
+                        } else {
+                            //console.log('New player: ' + p);
+                            req.login(p, function(err) {
+                                if (err) { return next(err); }
+                                req.flash('success_msg', 'Welcome ' + p.handle + "!");
+                                return res.redirect('/arena');
+                            });
+                        }
+                    });
+                } else {
+                    req.flash('error_msg', 'Re-typed password did not match the original.');
+                    res.redirect('/register');
+                }
             }
         }
     });
